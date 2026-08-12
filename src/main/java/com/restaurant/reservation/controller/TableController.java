9154ff41906ec6f102851831e67808354e1c00cb
canastillo <66333350+canastillo@users.noreply.github.com>
2026-08-12 17:22:46 -0600

    refactor: rename table controller and test classes

--- src/main/java/com/restaurant/reservation/controller/TableController.java
+++ src/main/java/com/restaurant/reservation/controller/TableController.java
@@ -4,6 +4,7 @@ import com.restaurant.reservation.domain.enums.TableType;
 import com.restaurant.reservation.dto.MessageResponse;
 import com.restaurant.reservation.dto.TableInitializationRequest;
 import com.restaurant.reservation.service.TableService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import java.util.Map;
 @RequestMapping("/api/admin/tables")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Admin operations", description = "Configuración inicial del salón")
 public class AdminController {
     private final TableService tableService;
 
@@ -24,5 +26,12 @@ public class AdminController {
         log.info("Received POST request for tables.");
         tableService.initializeTables(request);
         return ResponseEntity.ok(new MessageResponse("Tables configured successfully."));
+    }
+
+    @GetMapping()
+    public ResponseEntity<Map<TableType, Integer>> getRestaurantTables() {
+        log.info("Received GET request for tables.");
+        Map<TableType, Integer> allTables = tableService.getAllTables();
+        return ResponseEntity.ok(allTables);
     }
 }
