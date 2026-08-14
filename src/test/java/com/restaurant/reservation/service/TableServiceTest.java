5576bcfe09af6b166a5ff84d2c16e6f490d0a549
canastillo <66333350+canastillo@users.noreply.github.com>
2026-08-13 20:07:10 -0600

    ci: remove datasource env vars from CI to use H2 in tests

--- src/test/java/com/restaurant/reservation/service/TableServiceTest.java
+++ src/test/java/com/restaurant/reservation/service/TableServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class TableServiceTest {
 
     @Mock
