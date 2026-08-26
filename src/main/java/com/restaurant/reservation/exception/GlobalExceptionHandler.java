df9c58eaea7e16ad7ca71db68fa0c084f0e8cad1
canastillo <66333350+canastillo@users.noreply.github.com>
2026-08-25 20:27:44 -0600

    refactor: centralize UTC timestamp and correlationId in ApiError constructor

--- src/main/java/com/restaurant/reservation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/restaurant/reservation/exception/GlobalExceptionHandler.java
@@ -1,15 +1,11 @@
 package com.restaurant.reservation.exception;
 
-import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -40,9 +36,7 @@ public class GlobalExceptionHandler {
         // TODO: Log actual error for internal debugging
         ApiError error = new ApiError(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "Internal error in server",
-                OffsetDateTime.now(ZoneOffset.UTC),
-                MDC.get(CORRELATION_ID_KEY)
+                "Internal error in server"
         );
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -50,9 +44,7 @@ public class GlobalExceptionHandler {
     private ResponseEntity<ApiError> createBadRequestResponse(String message) {
         ApiError error = new ApiError(
                 HttpStatus.BAD_REQUEST.value(),
-                message,
-                OffsetDateTime.now(ZoneOffset.UTC),
-                MDC.get(CORRELATION_ID_KEY)
+                message
         );
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
