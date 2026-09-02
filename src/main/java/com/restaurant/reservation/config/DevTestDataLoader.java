084ae810b964ad100787d258779e6d7202c43c2e
canastillo <66333350+canastillo@users.noreply.github.com>
2026-09-01 19:47:24 -0600

    chore: add logging to devtest loader class

--- src/main/java/com/restaurant/reservation/config/DevTestDataLoader.java
+++ src/main/java/com/restaurant/reservation/config/DevTestDataLoader.java
@@ -5,6 +5,7 @@ import com.restaurant.reservation.domain.models.Role;
 import com.restaurant.reservation.domain.models.User;
 import com.restaurant.reservation.repository.RoleRepository;
 import com.restaurant.reservation.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,7 @@ import java.util.Set;
  * Loads initial roles and users for development and test environments.
  * Activated only when the "dev" or "test" profile is active.
  */
+@Slf4j
 @Component
 @Profile({"dev", "test"})
 public class DevTestDataLoader implements CommandLineRunner {
@@ -36,15 +38,18 @@ public class DevTestDataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         createRolesIfNotFound();
         createDefaultUsersIfNotFound();
+        log.debug("Dummy data is ready in the database");
     }
 
     private void createRolesIfNotFound() {
         if (roleRepository.findByName(RoleType.ROLE_ADMIN).isEmpty()) {
             roleRepository.save(new Role(RoleType.ROLE_ADMIN));
+            log.debug("Role {} has been added to the database", RoleType.ROLE_ADMIN);
         }
 
         if (roleRepository.findByName(RoleType.ROLE_USER).isEmpty()) {
             roleRepository.save(new Role(RoleType.ROLE_USER));
+            log.debug("Role {} has been added to the database ", RoleType.ROLE_ADMIN);
         }
     }
 
@@ -53,12 +58,14 @@ public class DevTestDataLoader implements CommandLineRunner {
             User user = new User("Admin", "admin@admin.com", passwordEncoder.encode("admin"));
             user.setRoles(Set.of(roleRepository.findByName(RoleType.ROLE_ADMIN).orElseThrow()));
             userRepository.save(user);
+            log.debug("User of type {} has been created", RoleType.ROLE_ADMIN);
         }
 
         if (!userRepository.existsByEmail("user@user.com")) {
             User user = new User("User", "user@user.com", passwordEncoder.encode("user"));
             user.setRoles(Set.of(roleRepository.findByName(RoleType.ROLE_USER).orElseThrow()));
             userRepository.save(user);
+            log.debug("User of type {} has been created", RoleType.ROLE_USER);
         }
     }
 }
